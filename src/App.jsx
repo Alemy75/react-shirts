e57b258a554cc9d594e5ab2965eafcad01e895b6
Alemax75 <alemax75962@gmail.com>
2023-03-03 23:44:46 +0300

    feat: Верстка header

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,33 @@
+import CartIco from './assets/svg/cart.svg'
+import SearchIco from './assets/svg/search.svg'
+import LoginIco from './assets/svg/login.svg'
+import ProfileIco from './assets/svg/account.svg'
+
 function App() {
 
     return (
-        <div className="container">
-
+        <div className="header-wrap">
+            <div className="container">
+                <div className="header">
+                    <div className="header__logo">
+                        <div className="header__text">T-Shirts</div>
+                    </div>
+                    <ul className="header__nav">
+                        <li className="nav__item">ГЛАВНАЯ</li>
+                        <li className="nav__item">АКЦИИ</li>
+                        <li className="nav__item">О КОМПАНИИ</li>
+                        <li className="nav__item">ОТЗЫВЫ</li>
+                        <li className="nav__item">ДОСТАВКА И ОПЛАТА</li>
+                        <li className="nav__item">КОНТАКТЫ</li>
+                    </ul>
+                    <div className="header__links">
+                        <img src={CartIco} alt=""/>
+                        <img src={SearchIco} alt=""/>
+                        <img src={LoginIco} alt=""/>
+                        <img src={ProfileIco} alt=""/>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,9 @@ import App from './App'
 import './scss/main.scss'
 import * as ReactDOM from "react-dom";
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>,
 )
